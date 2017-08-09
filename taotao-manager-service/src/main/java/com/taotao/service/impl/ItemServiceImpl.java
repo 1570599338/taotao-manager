e355ad10f270b6b67bf3a127eea754210ae6da8d
lquan <1570599338@qq.com>
2017-08-10 00:03:33 +0800

    service商品信息

--- taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -1,5 +1,6 @@
 package com.taotao.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.IDUtils;
+import com.taotao.mapper.TbItemDescMapper;
 import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemDesc;
 import com.taotao.pojo.TbItemExample;
 import com.taotao.service.ItemService;
 
@@ -24,6 +29,9 @@ public class ItemServiceImpl implements ItemService {
 
 	@Autowired
 	private TbItemMapper itemMapper;
+	
+	@Autowired
+	private TbItemDescMapper tbItemDescMapper;
 	
 	/**
 	 * 根据商品信息查询商品信息
@@ -57,6 +65,36 @@ public class ItemServiceImpl implements ItemService {
 		result.setRows(list);
 		result.setTotal(pageInfo.getTotal());
 		return result;
+	}
+
+	/**
+	 * 添加商品信息
+	 * @param item 商品信息
+	 * @param desc 商品描述
+	 * @return
+	 */
+	@Override
+	public TaotaoResult addItem(TbItem item, String desc) {
+		// 生成商品id
+		Long id = IDUtils.genItemId();
+		//补全item的属性
+		item.setId(id);
+		item.setStatus((byte)1);
+		item.setCreated(new Date());
+		item.setUpdated(new Date());
+		//向商品表插入数据
+		itemMapper.insert(item);
+		//创建一个商品描述表对应的pojo
+		TbItemDesc tbItem = new TbItemDesc();
+		//补全pojo的属性
+		tbItem.setItemId(id);
+		tbItem.setItemDesc(desc);
+		tbItem.setCreated(new Date());
+		tbItem.setUpdated(new Date());
+		//向商品描述表插入数据
+		tbItemDescMapper.insert(tbItem);
+		// 返回结果
+		return TaotaoResult.ok();
 	}
 
 	
